57d39610a8aff8fb1a67fc82079dac8a463b32dc
Graig Janssen <graig.janssen@gmail.com>
2016-03-29 09:52:37 -0400

    Sets media query breakpoints, slider styling first steps

--- public/js/controllers/slider.js
+++ public/js/controllers/slider.js
@@ -3,6 +3,11 @@ var ctrl = angular.module('sliderCtrl', []);
 ctrl.controller('SliderController', ['$scope', 'booksApi', function($scope, booksApi){
   $scope.books = [];
 
+  // $scope.rotateBooks = function(){
+  //     var first = $scope.books.shift();
+  //     $scope.books.push(first);
+  // };
+
   function getBooks() {
     booksApi.getBooks().then(function(response){
       $scope.books = response.data.books;
