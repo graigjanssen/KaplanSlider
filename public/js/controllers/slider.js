5b2db351f4a876a42f9669345f184cb7da9fbb60
Graig Janssen <graig.janssen@gmail.com>
2016-03-30 18:32:43 -0400

    Play/pause function no animation

--- public/js/controllers/slider.js
+++ public/js/controllers/slider.js
@@ -1,12 +1,37 @@
 var ctrl = angular.module('sliderCtrl', []);
 
-ctrl.controller('SliderController', ['$scope', 'booksApi', function($scope, booksApi){
+ctrl.controller('SliderController', ['$scope', '$interval', 'booksApi', function($scope, $interval, booksApi){
   $scope.books = [];
 
-  // $scope.rotateBooks = function(){
-  //     var first = $scope.books.shift();
-  //     $scope.books.push(first);
-  // };
+  function rotateBooks() {
+      var first = $scope.books.shift();
+      $scope.books.push(first);
+  }
+
+  // Variable to store $interval promise object
+  var play;
+
+  $scope.toggleSlider = function(e){
+    // Toggle button play/pause icon
+    var el = e.target;
+    if (el.className === "fa fa-play-circle") {
+      el.className = "fa fa-pause-circle";
+      play = $interval(function(){
+        rotateBooks();
+      }, 1000);
+    } else {
+      if (angular.isDefined(play)) {
+        console.log('play defined');
+        $interval.cancel(play);
+        play = undefined;
+      }
+      el.className = "fa fa-play-circle";
+    }
+  };
+
+  $scope.resetSlider = function(){
+
+  };
 
   function getBooks() {
     booksApi.getBooks().then(function(response){
